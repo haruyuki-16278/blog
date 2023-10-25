560e5e0d543737dda19c3970413cc62de2a1ffca
haruyuki@gemibook-wsl <haruyuki@relicsnow.info>
2023-10-25 17:20:03 +0900

    コメントを追加

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-"""
-Flaskをインポート
-"""
-from flask import Flask, request, jsonify
 import uuid
 import json
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -25,61 +22,119 @@ def join() -> dict:
     GETならルーム状態の取得
     """
     if (request.method == "POST"):
+        # POSTのときの処理
+
+        # POSTリクエストのbodyにあるデータを取り出してデコード
+        # JSONとして解釈して辞書型的に使える形式にして data に代入
         data = json.loads(request.data.decode('utf-8'))
-        print(data)
-        print(rooms)
+        # 今あるルームのIDのリストを取得
         roomids = list(rooms.keys())
-        if (len(rooms.keys()) > 0 and len(rooms[roomids[-1]]['players']) == 1):
+
+        if (len(roomids) > 0\
+                and len(rooms[roomids[-1]]['players']) == 1):
+            # ルームが1以上あり、
+            # かつ最後のルームのプレイヤーが一人なら
+
+            # ルームに参加してステータスをプレイ中にする
             rooms[roomids[-1]]['players'].append(data['id'])
             rooms[roomids[-1]]['status'] = 'playing'
+            # 入ったルームを返す
             return {'roomId': roomids[-1]}
+        
         else:
+            # ルームが無い
+            # もしくは最後のルームのプレイヤーが一人でないなら
+
+            # ルームを新しく作成する
             roomid = str(uuid.uuid4())
-            print(roomid)
             rooms[roomid] = {
                 'players': [data['id']],
                 'shiritori': ['しりとり'],
                 'status': 'matching'
             }
+            # 入ったルームを返す
             return {'roomId': roomid}
+
     elif (request.method == "GET"):
+        # GETのときの処理
+
+        # クエリパラメータからルームIDを取得
         params = request.args.to_dict()
         roomid = params['roomid']
-        print(roomid, rooms[roomid]['players'])
-        ready = len(rooms[roomid]['players']) == 2
+
+        # ルームのステータスが 'playing' なら 'ready' がTrueに
+        # そうでないならFalseになる
+        ready = len(rooms[roomid]['status']) == 'playing'
+        # ルームの準備状況とメンバーを返す
         return {'ready': ready, 'member': rooms[roomid]['players']}
-    else:
-        return {'error': 'invalid request'}
 
 @app.route('/shiritori/<roomid>', methods=["POST", "GET"])
 def shiritori(roomid) -> dict:
+    """
+    しりとりをやり取りするAPI
+
+    POSTならしりとりに回答する
+    GETなら現在の最後の回答を取得する
+    """
     if (request.method == "POST"):
+        # POSTのときの処理
+
+        # POSTリクエストのbodyにあるデータを取り出してデコード
+        # JSONとして解釈して辞書型的に使える形式にして data に代入
         data = json.loads(request.data.decode('utf-8'))
+        # dataから回答を抜き出す
         answer = data['answer']
+
+        # ルーム情報からしりとりの履歴を取得
         shiritories = rooms[roomid]['shiritori']
         lastword = shiritories[-1]
+
+        # 最後のことばの最後の文字と回答の最初の文字が等しいかどうか
         is_same_last_letter_and_firtst_lettar = lastword[-1] == answer[0]
+        # 回答の最後の文字が ん であるかどうか
         is_last_letter_NN = answer[-1] == 'ん'
+        # 回答が既に使われたことばかどうか
         is_appeared = answer in shiritories
+
+        # ルームの履歴に回答を追加する
+        rooms[roomid]['shiritori'].append(answer)
+
         if (not is_same_last_letter_and_firtst_lettar\
                 or is_last_letter_NN\
                 or is_appeared):
+            # しりとりのルールを守れていない回答なら
+
+            # ルームのステータスを 'finished' にする
             rooms[roomid]['status'] = 'finished'
+            # 敗北したことを返す
             return {'result': 'defeat'}
+
         else:
-            rooms[roomid]['shiritori'].append(answer)
+            # しりとりのルールが守られている回答なら
+
+            # 回答が成功したことを返す
             return {'result': 'collect'}
+
     elif (request.method == "GET"):
+        # GETのときの処理
+
+        # ルームのしりとり履歴を取得
         shiritories = rooms[roomid]['shiritori']
+
         if (rooms[roomid]['status'] == 'finished'):
+            # ルームのステータスが 'finished' なら
+
+            # 勝利したことを返す
             return {'result': 'victory'}
-        else:
+
+        elif ((rooms[roomid]['status'] == 'playing')):
+            # ルームのステータスがプレイ中なら
+
+            # 現在の最後の回答を返す
             return {
                 'lastword': shiritories[-1],
                 'words': len(shiritories)
             }
-    else:
-        return {'error': 'invalid request'}
 
 if __name__ == '__main__':
     app.run(
